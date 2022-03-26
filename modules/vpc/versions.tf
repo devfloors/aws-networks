af80f1b5f7814b79f23ee0a69f9da8957e131796
devfloor <csh47790@gmail.com>
2022-03-26 22:32:38 +0900

    dependency versions

--- modules/vpc/versions.tf
+++ modules/vpc/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = ">= 1.0.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.45"
+      version = "~> 4.0"
     }
   }
 }
